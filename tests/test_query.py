2f60a55ce4d2528867c1d78aa9fff3007b2f9a2e
lsabi <13497689+lsabi@users.noreply.github.com>
2021-03-12 23:22:26 +0100

    :rotating_light: :construction: WIP query.py tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,1 +1,14 @@
 # Tests for query.py
+from unittest.mock import patch
+
+
+from rethinkdb.query import (
+  json
+)
+
+
+def test_json():
+  with patch("rethinkdb.query.json") as json_mock:
+    json_mock("foo")
+    
+    json_mock.assert_called_once_with("foo")

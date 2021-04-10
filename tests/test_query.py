6188f597372ef890fed31d8e19e4c2aeb3e5ef0f
lsabi <13497689+lsabi@users.noreply.github.com>
2021-04-10 21:57:17 +0200

    :white_check_mark: Start adding query.py tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -3,12 +3,31 @@ from unittest.mock import patch
 
 
 from rethinkdb.query import (
-  json
+  json,
+  js
 )
 
 
-def test_json():
+@patch("rethinkdb.query.ast")
+def test_json(mock_ast):
+  mock_ast.Json.return_value = Mock()
+  
+  result = json("foo")
+  mock_ast.assert_called_once_with("foo")
+  
+  assert result == mock_ast.Json.return_value
+  """"
   with patch("rethinkdb.query.json") as json_mock:
     json_mock("foo")
     
     json_mock.assert_called_once_with("foo")
+  """
+
+@patch("rethinkdb.query.ast")
+def test_js(mock_ast):
+  mock_ast.JavaScript.return_value = Mock()
+  
+  result = js("foo", foo="foo")
+  mock_ast.assert.called_once_with("foo", foo="foo")
+  
+  assert result == mock_ast.JavaScript.return_value
